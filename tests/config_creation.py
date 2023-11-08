8610926fffa4eaa02a4dd42fca3cd81766ad22e7
matteocao <38946719+matteocao@users.noreply.github.com>
2023-11-08 17:13:48 +0100

    Update and rename config_creation.py to test_config_creation.py

--- tests/config_creation.py
+++
@@ -1,9 +0,0 @@
-from pipeline_tool.pipeline_config import PipelineConfig
-
-config = PipelineConfig(input_shape=[1,2], output_shape=[12], data_type="long")
-
-print("A simple config as been created")
-
-config.create_mha_conf_equal(nb_mha=2, num_heads=12, embed_dim=3, dropout=0.0, batch_first=True)
-
-print("An MHA config as been added")
