0b9b2e0bcab369897e2cd1555dc67829c75e50bc
matteocao <38946719+matteocao@users.noreply.github.com>
2023-11-08 17:14:27 +0100

    Update and rename import.py to test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,4 +1,6 @@
 from pipeline_tool.pipeline_tool import SkippableTracing
 from pipeline_tool.pipeline_config import PipelineConfig
 
-print("All user import needed are working !")
+
+def test_import():
+    print("All user import needed are working !")
